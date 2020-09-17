d1c2be91aaabbbb516121c26b8bdbbd97a8af917
Pontanilla <calv.pontanilla@wsp.com>
2020-09-17 14:17:01 +0800

    Base surface

--- flappy.py
+++ flappy.py
@@ -1,5 +1,12 @@
 import pygame, sys
 
+# dedicate for mounting the base surface on the image
+def draw_base():
+	screen.blit(base_surface,(base_x_pos, 450))
+	screen.blit(base_surface,(base_x_pos + 288, 450))
+
+
+
 # initialize Pygame
 pygame.init()
 
@@ -9,6 +16,13 @@ screen = pygame.display.set_mode((288,512))
 clock = pygame.time.Clock()
 #background surface, image.load("directory").convert()
 bg_surface = pygame.image.load('assets/background-day.png').convert()
+# scale image to screen dimnesion
+##bg_surface = pygame.transform.scale2x(bg_surface)
+# base image
+base_surface = pygame.image.load('assets/base.png').convert()
+base_x_pos = 0
+
+
 
 #Implement the game loop
 while True:
@@ -16,8 +30,12 @@ while True:
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
-	# to position the bg-surface blit method in x-axis and y-axis
+	# to position the bg-surface blit method in x-axis and y-axis top left
 	screen.blit(bg_surface,(0,0))
+	base_x_pos -=1 # animation effect for the base x axis(+ move to right , - move to left)
+	draw_base()
+	if base_x_pos <= -288 :
+		base_x_pos = 0
 
 	pygame.display.update()
 	clock.tick(120) #Frame rate, fps #setting the frame rate
